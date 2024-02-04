ec9e5521e811fdbc9106adaf1f98214c2adaf2ac
Dylan Balagtas <dylansatgalab@gmail.com>
2024-02-05 01:43:12 +0800

    style to include buttons
    no functionality

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,13 +4,25 @@ import './App.css';
 
 function Hello() {
   return (
-    <div className='score'>
-      <div className='blue-score'>
-        <h1>20</h1>
+    <div className="grid">
+      <div>Timer</div>
+      <div className="score">
+        <div className="blue-score">
+          <div className="control">
+            <button type="button">+</button>
+            <button type="button">-</button>
+          </div>
+          <h1>20</h1>
+        </div>
+        <div className="red-score">
+          <h1>10</h1>
+          <div className="control">
+            <button type="button">+</button>
+            <button type="button">-</button>
+          </div>
+        </div>
       </div>
-      <div className='red-score'>
-        <h1>10</h1>
-      </div>
+      <div>footer</div>
     </div>
   );
 }
