eeae25b2a7f513e5ce74b2ce90c367b46194d809
Dylan Balagtas <dylansatgalab@gmail.com>
2024-02-05 02:23:31 +0800

    adds button functionality to increment and decrement score

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,24 +1,46 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { useState } from 'react';
 import './App.css';
 
 function Hello() {
+  const [blueScore, setBlueScore] = useState(20); // Initialize blueScore state
+  const [redScore, setRedScore] = useState(20); // Initialize blueScore state
+  function increaseBlueScore() {
+    setBlueScore(blueScore + 1);
+  }
+  function decreaseBlueScore() {
+    setBlueScore(blueScore - 1);
+  }
+  function increaseRedScore() {
+    setRedScore(redScore + 1);
+  }
+  function decreaseRedScore() {
+    setRedScore(redScore - 1);
+  }
   return (
     <div className="grid">
       <div>Timer</div>
       <div className="score">
         <div className="blue-score">
           <div className="control">
-            <button type="button">+</button>
-            <button type="button">-</button>
+            <button type="button" onClick={increaseBlueScore}>
+              +
+            </button>
+            <button type="button" onClick={decreaseBlueScore}>
+              -
+            </button>
           </div>
-          <h1>20</h1>
+          <h1>{blueScore}</h1>
         </div>
         <div className="red-score">
-          <h1>10</h1>
+          <h1>{redScore}</h1>
           <div className="control">
-            <button type="button">+</button>
-            <button type="button">-</button>
+            <button type="button" onClick={increaseRedScore}>
+              +
+            </button>
+            <button type="button" onClick={decreaseRedScore}>
+              -
+            </button>
           </div>
         </div>
       </div>
