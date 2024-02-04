3b4ac562584849f746b80c87874e2a8d21dac210
Dylan Balagtas <dylansatgalab@gmail.com>
2024-02-04 16:54:51 +0800

    initial design with displaying blue and red score

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,36 +4,12 @@ import './App.css';
 
 function Hello() {
   return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
+    <div className='score'>
+      <div className='blue-score'>
+        <h1>20</h1>
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+      <div className='red-score'>
+        <h1>10</h1>
       </div>
     </div>
   );
