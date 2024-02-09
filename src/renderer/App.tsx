e19149fcd5168cd19aada4d5db13dd52c59839d7
Dylan Balagtas <dylansatgalab@gmail.com>
2024-02-10 03:03:19 +0800

    timer for fight rounds, with control

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,62 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import './App.css';
+import { useTimer } from 'react-timer-hook';
+
+function MyTimer({ expiryTimestamp }) {
+  const {
+    totalSeconds,
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({
+    expiryTimestamp,
+    onExpire: () => console.warn('onExpire called'),
+  });
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <div style={{ fontSize: '100px' }}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:
+        <span>{seconds}</span>
+      </div>
+      <p>{isRunning ? 'Running' : 'Not running'}</p>
+      <button onClick={start} type="button">
+        Start
+      </button>
+      <button onClick={pause} type="button">
+        Pause
+      </button>
+      <button onClick={resume} type="button">
+        Resume
+      </button>
+      <button
+        onClick={() => {
+          // Restarts to 5 minutes timer
+          const time = new Date();
+          time.setSeconds(time.getSeconds() + 300);
+          restart(time, false);
+        }}
+        type="button"
+      >
+        Restart
+      </button>
+    </div>
+  );
+}
 
 function Hello() {
   const [blueScore, setBlueScore] = useState(20); // Initialize blueScore state
   const [redScore, setRedScore] = useState(20); // Initialize blueScore state
+
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+
   function increaseBlueScore() {
     setBlueScore(blueScore + 1);
   }
@@ -19,7 +71,9 @@ function Hello() {
   }
   return (
     <div className="grid">
-      <div>Timer</div>
+      <div className="timer">
+        <MyTimer expiryTimestamp={time} />
+      </div>
       <div className="score">
         <div className="blue-score">
           <div className="control">
