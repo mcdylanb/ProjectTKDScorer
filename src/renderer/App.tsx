fc88b28021eea1f6c119c626b332c8fb2b4d0d69
Dylan Balagtas <dylansatgalab@gmail.com>
2024-02-12 02:19:34 +0800

    preperation to move button changes in different containers

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,7 +3,18 @@ import { useState } from 'react';
 import './App.css';
 import { useTimer } from 'react-timer-hook';
 
-function MyTimer({ expiryTimestamp }) {
+function Hello(expiryTimestamp) {
+  const [blueScore, setBlueScore] = useState(20); // Initialize blueScore state
+  const [redScore, setRedScore] = useState(20); // Initialize blueScore state
+  const [gameMode, setGameMode] = useState('BREAK'); // Initialize blueScore state
+  //round winner tracker
+  const [round1Winner, setRound1Winner] = useState('white');
+  const [round2Winner, setRound2Winner] = useState('white');
+  const [round3Winner, setRound3Winner] = useState('white');
+
+  const [blueGJCounter, setBlueGJCounter] = useState(0);
+  const [redGJCounter, setRedGJCounter] = useState(0);
+
   const {
     totalSeconds,
     seconds,
@@ -19,69 +30,6 @@ function MyTimer({ expiryTimestamp }) {
     expiryTimestamp,
     onExpire: () => console.warn('onExpire called'),
   });
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <div style={{ fontSize: '100px' }}>
-        <span>{minutes}</span>:<span>{seconds}</span>
-      </div>
-      <p>{isRunning ? 'Running' : 'Not running'}</p>
-      <button onClick={start} type="button">
-        Start
-      </button>
-      <button onClick={pause} type="button">
-        Pause
-      </button>
-      <button onClick={resume} type="button">
-        Resume
-      </button>
-      <button
-        onClick={() => {
-          // Restarts to 5 minutes timer
-          const time = new Date();
-          time.setSeconds(time.getSeconds() + 300);
-          restart(time, false);
-        }}
-        type="button"
-      >
-        Restart
-      </button>
-      <button
-        onClick={() => {
-          const time = new Date();
-          time.setSeconds(time.getSeconds() + 90);
-          restart(time, false);
-        }}
-        type="button"
-      >
-        Game
-      </button>
-      <button
-        onClick={() => {
-          const time = new Date();
-          time.setSeconds(time.getSeconds() + 60);
-          restart(time, false);
-        }}
-        type="button"
-      >
-        Break
-      </button>
-    </div>
-  );
-}
-
-function Hello() {
-  const [blueScore, setBlueScore] = useState(20); // Initialize blueScore state
-  const [redScore, setRedScore] = useState(20); // Initialize blueScore state
-  //round winner tracker
-  const [round1Winner, setRound1Winner] = useState('white');
-  const [round2Winner, setRound2Winner] = useState('white');
-  const [round3Winner, setRound3Winner] = useState('white');
-
-  const [blueGJCounter, setBlueGJCounter] = useState(0);
-  const [redGJCounter, setRedGJCounter] = useState(0);
-
-  const time = new Date();
-  time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
 
   //increment and decrement score of each player
   function increaseBlueScore() {
@@ -129,32 +77,153 @@ function Hello() {
   return (
     <div className="grid">
       <div className="blue-flag">empty</div>
-      <div className="blue-name">BNAME</div>
-      <div className="red-name">RNAME</div>
+      <div className="blue-name">
+        <h1>BALAGTAS</h1>
+      </div>
+      <div className="red-name">
+        <h1>LAYAGUE</h1>
+      </div>
       <div className="red-flag">empty</div>
 
-      <div className="blue-score-control">BS CONTROL</div>
-      <div className="red-score-control">RS CONTROL</div>
+      <div className="blue-score-control-container">
+        <div className="control">
+          <button type="button" onClick={increaseBlueScore}>
+            BLUE SCORE +
+          </button>
+          <button type="button" onClick={decreaseBlueScore}>
+            BLUE SCORE -
+          </button>
+          {/* Blue GJ Counter  */}
+          <button type="button" onClick={increaseBlueGJCounter}>
+            BLUE GJ +
+          </button>
+          <button type="button" onClick={decreaseBlueGJCounter}>
+            BLUE GJ -
+          </button>
+        </div>
+      </div>
+      <div className="red-score-control-container">
+        <div className="control">
+          <button type="button" onClick={increaseRedScore}>
+            RED SCORE +
+          </button>
+          <button type="button" onClick={decreaseRedScore}>
+            RED SCORE -
+          </button>
+          {/* Red GJ Counter  */}
+          <button type="button" onClick={increaseRedGJCounter}>
+            RED GJ +
+          </button>
+          <button type="button" onClick={decreaseRedGJCounter}>
+            RED GJ -
+          </button>
+        </div>
+      </div>
+      <div className="blue-score-container">
+        <h1>{blueScore} </h1>
+      </div>
+      <div className="red-score-container">
+        <h1>{redScore} </h1>
+      </div>
 
-      <div className="blue-score">BS SCORE</div>
-      <div className="game-mode">GAME MODE</div>
-      <div className="timer">TIMER</div>
-      <div className="timer-control">TIMER CONTROLLER</div>
-      <div className="red-score">RS SCORE</div>
-
-      <div className="blue-gj">BGJ</div>
-      <div className="game-mode-control">GAME CONTROL</div>
-      <div className="red-gj">RGJ</div>
+      <div className="game-mode">
+        <h1>{gameMode}</h1>
+      </div>
+      {/* TIMER */}
+      <div className="timer">
+        {' '}
+        <div className="roundTracker">
+          <div
+            className="round1"
+            onClick={() => changeRoundWinner(round1Winner, setRound1Winner)}
+            style={{ backgroundColor: `${round1Winner}` }}
+          >
+            R1
+          </div>
+          <div
+            className="round2"
+            onClick={() => changeRoundWinner(round2Winner, setRound2Winner)}
+            style={{ backgroundColor: `${round2Winner}` }}
+          >
+            R2
+          </div>
+          <div
+            className="round3"
+            onClick={() => changeRoundWinner(round3Winner, setRound3Winner)}
+            style={{ backgroundColor: `${round3Winner}` }}
+          >
+            R3
+          </div>
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          <div style={{ fontSize: '100px' }}>
+            <span>{minutes}</span>:<span>{seconds}</span>
+          </div>
+          <p>{isRunning ? 'Running' : 'Not running'}</p>
+          <button onClick={start} type="button">
+            Start
+          </button>
+          <button onClick={pause} type="button">
+            Pause
+          </button>
+          <button onClick={resume} type="button">
+            Resume
+          </button>
+          <button
+            onClick={() => {
+              // Restarts to 5 minutes timer
+              const time = new Date();
+              time.setSeconds(time.getSeconds() + 300);
+              restart(time, false);
+            }}
+            type="button"
+          >
+            Restart
+          </button>
+          <button
+            onClick={() => {
+              const time = new Date();
+              time.setSeconds(time.getSeconds() + 90);
+              restart(time, false);
+            }}
+            type="button"
+          >
+            Game
+          </button>
+          <button
+            onClick={() => {
+              const time = new Date();
+              time.setSeconds(time.getSeconds() + 60);
+              restart(time, false);
+            }}
+            type="button"
+          >
+            Break
+          </button>
+        </div>
+      </div>
+      <div className="timer-control">timer-control</div>
+      <div className="blue-gj">
+        <h3>GAM-JEOM</h3>
+        <h1>{blueGJCounter}</h1>
+      </div>
+      <div className="red-gj">
+        <h3>GAM-JEOM</h3>
+        <h1>{redGJCounter}</h1>
+      </div>
+      <div className="game-mode-control">GAME MODE CONTROL</div>
+      <div className="blue-footer">footer</div>
+      <div className="red-footer">footer</div>
     </div>
   );
 }
 
 export default function App() {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
+    <div>
+      <Hello expiryTimestamp={time} />
+    </div>
   );
 }
