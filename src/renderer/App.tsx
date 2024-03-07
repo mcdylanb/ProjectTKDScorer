e5e905f036e272b8e210d8b9fea7943f8cbed36a
Dylan Balagtas <dylansatgalab@gmail.com>
2024-03-07 14:25:36 +0800

    RoundTracker move to own component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,14 +3,53 @@ import { useState } from 'react';
 import './App.css';
 import { useTimer } from 'react-timer-hook';
 
+function RoundTracker() {
+  const [round1Winner, setRound1Winner] = useState('white');
+  const [round2Winner, setRound2Winner] = useState('white');
+  const [round3Winner, setRound3Winner] = useState('white');
+  function changeRoundWinner(roundWinner: string, setRoundWinner: Function) {
+    if (roundWinner === 'white') {
+      setRoundWinner('blue');
+      console.log('winner blue');
+    } else if (roundWinner === 'blue') {
+      setRoundWinner('red');
+      console.log('winner red');
+    } else {
+      setRoundWinner('white');
+      console.log('winner none');
+    }
+  }
+  return (
+    <div className="roundTracker">
+      <div
+        className="round1"
+        onClick={() => changeRoundWinner(round1Winner, setRound1Winner)}
+        style={{ backgroundColor: `${round1Winner}` }}
+      >
+        R1
+      </div>
+      <div
+        className="round2"
+        onClick={() => changeRoundWinner(round2Winner, setRound2Winner)}
+        style={{ backgroundColor: `${round2Winner}` }}
+      >
+        R2
+      </div>
+      <div
+        className="round3"
+        onClick={() => changeRoundWinner(round3Winner, setRound3Winner)}
+        style={{ backgroundColor: `${round3Winner}` }}
+      >
+        R3
+      </div>
+    </div>
+  );
+}
 function Hello(expiryTimestamp) {
   const [blueScore, setBlueScore] = useState(20); // Initialize blueScore state
   const [redScore, setRedScore] = useState(20); // Initialize blueScore state
   const [gameMode, setGameMode] = useState('BREAK'); // Initialize blueScore state
   //round winner tracker
-  const [round1Winner, setRound1Winner] = useState('white');
-  const [round2Winner, setRound2Winner] = useState('white');
-  const [round3Winner, setRound3Winner] = useState('white');
 
   const [blueGJCounter, setBlueGJCounter] = useState(0);
   const [redGJCounter, setRedGJCounter] = useState(0);
@@ -240,31 +279,8 @@ function Hello(expiryTimestamp) {
       </div>
       {/* TIMER */}
       <div className="timer">
+        <RoundTracker />
         {' '}
-        <div className="roundTracker">
-          // TODO: move into component
-          <div
-            className="round1"
-            onClick={() => changeRoundWinner(round1Winner, setRound1Winner)}
-            style={{ backgroundColor: `${round1Winner}` }}
-          >
-            R1
-          </div>
-          <div
-            className="round2"
-            onClick={() => changeRoundWinner(round2Winner, setRound2Winner)}
-            style={{ backgroundColor: `${round2Winner}` }}
-          >
-            R2
-          </div>
-          <div
-            className="round3"
-            onClick={() => changeRoundWinner(round3Winner, setRound3Winner)}
-            style={{ backgroundColor: `${round3Winner}` }}
-          >
-            R3
-          </div>
-        </div>
         <div style={{ textAlign: 'center' }}>
           <div style={{ fontSize: '100px' }}>
             <span>{minutes}</span>:<span>{seconds}</span>
